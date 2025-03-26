823a67f415b484952ee034b33a5a40aeb0016b2a
Erkinbek Diushekeev <edyshekeev@gmail.com>
2025-03-26 17:41:08 +0600

    Map location

---
+++ src/app/components/Map.js
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const Map = () => {
+  const [position, setPosition] = useState([51.505, -0.09]); // Default location: London
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        setPosition([pos.coords.latitude, pos.coords.longitude]);
+      });
+    }
+  }, []);
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: "500px", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>Your Current Location</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
